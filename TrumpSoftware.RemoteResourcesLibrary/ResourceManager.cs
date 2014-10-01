2bcc46974f299d8f20798eb64fb9cec1ebe75e3a
Novikov Egor <trumpsoftware@gmail.com>
2014-10-02 00:29:53 +0400

    ResourceManager.IsLoaded was made public

--- TrumpSoftware.RemoteResourcesLibrary/ResourceManager.cs
+++ TrumpSoftware.RemoteResourcesLibrary/ResourceManager.cs
@@ -8,13 +8,13 @@ namespace TrumpSoftware.RemoteResourcesLibrary
     public sealed class ResourceManager
     {
         private readonly IDictionary<string, Resource> _resources = new Dictionary<string, Resource>();
-        private bool _isLoaded;
         private readonly Index _index;
 
         internal Uri CompiledResourceFolderUri { get; private set; }
         internal Uri LocalResourcesFolderUri { get; private set; }
         internal Uri RemoteResourcesFolderUri { get; private set; }
         public bool HasInternetConnection { get; private set; }
+        public bool IsLoaded;
 
         public ResourceManager(Uri compiledResourceFolderUri, Uri localResourcesFolderUri, Uri remoteResourcesFolderUri, string indexFileName = @"index.txt")
         {
@@ -32,14 +32,14 @@ namespace TrumpSoftware.RemoteResourcesLibrary
             _index = new Index(this, indexFileName);
         }
 
-        public async Task LoadIndex()
+        public async Task<bool> LoadIndex()
         {
             _resources.Clear();
             await _index.Load();
 
             if (!_index.IsLocalResourceInfoLoaded)
             {
-                _isLoaded = false;
+                IsLoaded = false;
                 return;
             }
 
@@ -60,13 +60,13 @@ namespace TrumpSoftware.RemoteResourcesLibrary
             }
 
             HasInternetConnection = _index.IsRemoteResourceInfoDownloaded;
-            _isLoaded = true;
+            IsLoaded = true;
         }
 
         public async Task<T> GetResource<T>(string path)
             where T : class
         {
-            if (!_isLoaded)
+            if (!IsLoaded)
                 return null;
             if (!_resources.ContainsKey(path))
                 return null;
@@ -77,7 +77,7 @@ namespace TrumpSoftware.RemoteResourcesLibrary
 
         public async Task LoadResource(string path)
         {
-            if (!_isLoaded)
+            if (!IsLoaded)
                 return;
             if (!_resources.ContainsKey(path))
                 return;
