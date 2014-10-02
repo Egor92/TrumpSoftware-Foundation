7d7f6696832012d01c700af83a43265422fcdf7f
Novikov Egor <trumpsoftware@gmail.com>
2014-10-02 18:07:58 +0400

    WinRT test app added

---
+++ TrumpSoftware.RemoteResourcesLibrary/ResourceFolderLocations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TrumpSoftware.RemoteResourcesLibrary
+{
+    internal class ResourceFolderLocations
+    {
+        internal Uri Compiled { get; private set; }
+        internal Uri Local { get; private set; }
+        internal Uri Remote { get; private set; }
+
+        internal ResourceFolderLocations(Uri compiled, Uri local, Uri remote)
+        {
+            Compiled = compiled;
+            Local = local;
+            Remote = remote;
+        }
+    }
+}
