21298bf4b404b8bc77df6407dbfa9b30a6576cf9
Novikov Egor <trumpsoftware@gmail.com>
2014-11-30 00:12:44 +0400

    VisualTreeHelperExtended and ICommunicator added

---
+++ TrumpSoftware.Wpf/VisualTreeHelperExtended.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace TrumpSoftware.Wpf
+{
+    public static class VisualTreeHelperExtended
+    {
+        public static T GetAncestor<T>(this DependencyObject source, bool includingItself = false)
+            where T : DependencyObject
+        {
+            return GetAncestor<T>(source, x => false, includingItself);
+        }
+
+        public static T GetAncestor<T>(this DependencyObject source, Func<DependencyObject, bool> stopCondition,  bool includingItself = false)
+            where T : DependencyObject
+        {
+            if (includingItself)
+            {
+                var tSource = source as T;
+                if (tSource != null)
+                    return tSource;
+            }
+            var parent = VisualTreeHelper.GetParent(source);
+            if (parent == null)
+                return null;
+            if (stopCondition(parent))
+                return null;
+            var tParent = parent as T;
+            if (tParent != null)
+                return tParent;
+            return GetAncestor<T>(parent, stopCondition);
+        }
+    }
+}
---
+++ TrumpSoftware.Xaml/Mvvm/ICommunicator.cs
@@ -0,0 +1,7 @@
+﻿namespace TrumpSoftware.Xaml.Mvvm
+{
+    public interface ICommunicator<out TAnswer, in TMessage>
+    {
+        TAnswer GetResponse(TMessage message = default(TMessage));
+    }
+}
