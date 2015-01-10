085987e96715aec29cb68a6a3ec7fa80bafe0179
Novikov Egor <trumpsoftware@gmail.com>
2015-01-10 17:38:38 +0300

    ListExtensions.Sort implemented

---
+++ TrumpSoftware.Common/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TrumpSoftware.Common
+{
+    public static class ListExtensions
+    {
+        public static void Sort<T>(this IList<T> sourceCollection, IComparer<T> comparer)
+        {
+            var list = new List<T>(sourceCollection);
+            list.Sort(comparer);
+            sourceCollection.Clear();
+            foreach (var item in list)
+                sourceCollection.Add(item);
+        }
+    }
+}
