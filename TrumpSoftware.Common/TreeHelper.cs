fd2ab0d4aa2adab4b92901e5c440b50a604c022d
Egor Novikov <trumpsoftware@gmail.com>
2015-03-22 17:57:28 +0300

    TreeHelper.TransformTree method fixed

--- TrumpSoftware.Common/TreeHelper.cs
+++ TrumpSoftware.Common/TreeHelper.cs
@@ -6,54 +6,57 @@ namespace TrumpSoftware.Common
 {
     public static class TreeHelper
     {
-        public static int GetNodeCount<T>(T source, Func<T, IEnumerable<T>> getChildren)
+        public static int GetNodeCount<T>(T sourceRoot, Func<T, IEnumerable<T>> getChildren)
             where T : class
         {
             if (getChildren == null) throw new ArgumentNullException("getChildren");
-            if (source == null)
+            if (sourceRoot == null)
                 return 0;
-            return 1 + getChildren(source).Sum(child => GetNodeCount(child, getChildren));
+            return 1 + getChildren(sourceRoot).Sum(child => GetNodeCount(child, getChildren));
         }
 
-        public static TTo Transform<TFrom, TTo>(TFrom source, Func<TFrom, TTo> transformNode, Func<TFrom, IEnumerable<TFrom>> getSourceChildren, Func<TTo, ICollection<TTo>> getTargetChildrenCollection, Action<TTo, TFrom> internalNodeAction, Action<TTo, TFrom> leafNodeAction)
+        public static TTo TransformTree<TFrom, TTo>(TFrom sourceRoot, Func<TFrom, TTo> transformNode, Func<TFrom, IEnumerable<TFrom>> getSourceChildren, Func<TTo, ICollection<TTo>> getTargetChildrenCollection, Action<TTo, TFrom> internalNodeAction, Action<TTo, TFrom> leafNodeAction)
             where TFrom : class
             where TTo : class
         {
             if (transformNode == null) throw new ArgumentNullException("transformNode");
             if (getSourceChildren == null) throw new ArgumentNullException("getSourceChildren");
             if (getTargetChildrenCollection == null) throw new ArgumentNullException("getTargetChildrenCollection");
-            if (source == null)
+            if (sourceRoot == null)
                 return null;
-            var target = transformNode(source);
-            if (target != null)
+            var targetRoot = transformNode(sourceRoot);
+            if (targetRoot != null)
             {
-                var sourceChildren = getSourceChildren(source);
+                var sourceChildren = getSourceChildren(sourceRoot);
                 if (sourceChildren != null)
                 {
-                    var targetChildrenCollection = getTargetChildrenCollection(target);
+                    var targetChildrenCollection = getTargetChildrenCollection(targetRoot);
                     if (targetChildrenCollection == null)
                         throw new Exception("targetChildrenCollection must be not null");
                     foreach (var sourceChild in sourceChildren)
                     {
-                        var targetChild = Transform(sourceChild, transformNode, getSourceChildren, getTargetChildrenCollection, internalNodeAction, leafNodeAction);
+                        var targetChild = TransformTree(sourceChild, transformNode, getSourceChildren, getTargetChildrenCollection, internalNodeAction, leafNodeAction);
                         targetChildrenCollection.Add(targetChild);
                     }
 
                     if (sourceChildren.Any())
                     {
-                        internalNodeAction(target, source);
+                        if (internalNodeAction != null)
+                            internalNodeAction(targetRoot, sourceRoot);
                     }
                     else
                     {
-                        leafNodeAction(target, source);
+                        if (leafNodeAction != null)
+                            leafNodeAction(targetRoot, sourceRoot);
                     }
                 }
                 else
                 {
-                    leafNodeAction(target, source);
+                    if (leafNodeAction != null)
+                        leafNodeAction(targetRoot, sourceRoot);
                 }
             }
-            return target;
+            return targetRoot;
         }
     }
 }
