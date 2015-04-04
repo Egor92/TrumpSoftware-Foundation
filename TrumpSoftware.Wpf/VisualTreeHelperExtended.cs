66d1dd04dcf22cef98cc27ec7193dee6186b3df4
Egor Novikov <trumpsoftware@gmail.com>
2015-04-04 22:14:35 +0300

    Methods for descendants implemented to VisualTreeHelperExtended

--- TrumpSoftware.Wpf/VisualTreeHelperExtended.cs
+++ TrumpSoftware.Wpf/VisualTreeHelperExtended.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
 
@@ -30,6 +31,71 @@ namespace TrumpSoftware.Wpf
             if (tParent != null)
                 return tParent;
             return GetAncestor<T>(parent, abortCondition);
+        }
+
+        public static IEnumerable<T> GetDescendants<T>(this DependencyObject source, bool includingItself = false)
+            where T : DependencyObject
+        {
+            return GetDescendants<T>(source, x => true, includingItself);
+        }
+
+        public static IEnumerable<T> GetDescendants<T>(this DependencyObject source, Func<T, bool> condition, bool includingItself = false)
+            where T : DependencyObject
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            var results = new List<T>();
+            if (includingItself)
+            {
+                var t = source as T;
+                if (t != null && (condition(t)))
+                    results.Add(t);
+            }
+            FindDescendants(source, condition, results);
+            return results;
+        }
+
+        private static void FindDescendants<T>(this DependencyObject source, Func<T, bool> condition, ICollection<T> results)
+            where T : DependencyObject
+        {
+            var childrenCount = VisualTreeHelper.GetChildrenCount(source);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(source, i);
+                var t = child as T;
+                if (t != null && (condition(t)))
+                    results.Add(t);
+                FindDescendants(child, condition, results);
+            }
+        }
+
+        public static T GetFirstDescendant<T>(this DependencyObject source, Func<T, bool> condition, bool includingItself = false)
+            where T : DependencyObject
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (includingItself)
+            {
+                var t = source as T;
+                if (t != null && (condition(t)))
+                    return t;
+            }
+            var childrenCount = VisualTreeHelper.GetChildrenCount(source);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(source, i);
+                var t = child as T;
+                if (t != null && (condition(t)))
+                    return t;
+            }
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(source, i);
+                var descendant = GetFirstDescendant(child, condition);
+                if (descendant != null)
+                    return descendant;
+            }
+            return null;
         }
     }
 }
