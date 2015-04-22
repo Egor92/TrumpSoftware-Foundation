b18b776425cd933a3266c07c67bb31e78a5c372d
Egor Novikov <trumpsoftware@gmail.com>
2015-04-22 20:47:20 +0300

    Ref class implemented

---
+++ TrumpSoftware.Common/Ref.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TrumpSoftware.Common
+{
+    public sealed class Ref<T>
+    {
+        private readonly Func<T> _getter;
+        private readonly Action<T> _setter;
+
+        public T Value
+        {
+            get { return _getter(); } 
+            set { _setter(value); }
+        }
+
+        public Ref(Func<T> getter, Action<T> setter)
+        {
+            _getter = getter;
+            _setter = setter;
+        }
+    }
+}
