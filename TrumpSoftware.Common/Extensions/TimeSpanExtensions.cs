976c97abf778d9a160d322d886be80f621146c57
Egor Novikov <trumpsoftware@gmail.com>
2015-08-05 22:56:14 +0300

    TimeSpanExtensions class added

---
+++ TrumpSoftware.Common/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TrumpSoftware.Common.Extensions
+{
+    public static class TimeSpanExtensions
+    {
+        public static TimeSpan Mult(this TimeSpan source, double multiplier)
+        {
+            var targetTicks = (long)(source.Ticks * multiplier);
+            return TimeSpan.FromTicks(targetTicks);
+        }
+    }
+}
