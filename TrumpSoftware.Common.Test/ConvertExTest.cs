6020f9b19a31e278843aa7d60dc3a9b644d0c914
Egor Novikov <trumpsoftware@gmail.com>
2015-12-10 23:02:59 +0300

    ConvertEc implemented

---
+++ TrumpSoftware.Common.Test/ConvertExTest.cs
@@ -0,0 +1,260 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrumpSoftware.Common.Test
+{
+    public class Descendant { }
+    public class Ancestor : Descendant { }
+
+    [TestClass]
+    public class ConvertExTest
+    {
+        [TestMethod]
+        public void CanConvertObjectToObject()
+        {
+            var value = new object();
+            CanConvert<object>(value, value);
+        }
+
+        [TestMethod]
+        public void CanConvertNullToObject()
+        {
+            CanConvert<object>(null, null);
+        }
+
+        [TestMethod]
+        public void CanConvertAncestorToDescendant()
+        {
+            var ancestor = new Ancestor();
+            CanConvert<Descendant>(ancestor, ancestor);
+        }
+
+        [TestMethod]
+        public void CannotConvertDescendantToAncestor()
+        {
+            CannotConvert<Ancestor>(new Descendant());
+        }
+
+        [TestMethod]
+        public void CannotConvertNotConvertibleClassToStruct()
+        {
+            CannotConvert<int>(new Descendant());
+        }
+
+        [TestMethod]
+        public void CannotConvertStructToNotConvertibleClass()
+        {
+            CannotConvert<Descendant>(4);
+        }
+
+        [TestMethod]
+        public void CanConvertDoubleToInt32()
+        {
+            CanConvert<int>(4.3, 4);
+        }
+
+        [TestMethod]
+        public void CanConvertNegativeDoubleToInt32()
+        {
+            CanConvert<int>(-4.3, -4);
+        }
+
+        [TestMethod]
+        public void CanConvertInt32ToDouble()
+        {
+            CanConvert<double>(4, 4.0);
+        }
+
+        [TestMethod]
+        public void CanConvertStringToInt32()
+        {
+            CanConvert<int>("4", 4);
+        }
+
+        [TestMethod]
+        public void CanConvertStringToDouble()
+        {
+            CanConvert<double>("4,3", 4.3);
+        }
+
+        [TestMethod]
+        public void CannotConvertEmptyStringToInt32()
+        {
+            CannotConvert<int>(string.Empty);
+        }
+
+        [TestMethod]
+        public void CannotConvertInvalidStringToInt32()
+        {
+            CannotConvert<int>("qqq");
+        }
+
+        [TestMethod]
+        public void CanConvertInt32ToString()
+        {
+            CanConvert<string>(4, "4");
+        }
+
+        [TestMethod]
+        public void CanConvertDoubleToString()
+        {
+            CanConvert<string>(4.3, "4,3");
+        }
+
+        [TestMethod]
+        public void CanConvertNullableInt32ToString()
+        {
+            CanConvert<string>(new int?(4), "4");
+        }
+
+        [TestMethod]
+        public void CanConvertNullableDoubleToString()
+        {
+            CanConvert<string>(new double?(4), "4");
+        }
+
+        [TestMethod]
+        public void CanConvertNullOfNullableToString()
+        {
+            CanConvert<string>((double?)null, null);
+        }
+
+        [TestMethod]
+        public void CanConvertStringToNullableInt32()
+        {
+            CanConvert<int?>("4", 4);
+        }
+
+        [TestMethod]
+        public void CanConvertStringToNullableDouble()
+        {
+            CanConvert<double?>("4,3", 4.3);
+        }
+
+        [TestMethod]
+        public void CannotConvertNullToDouble()
+        {
+            CannotConvert<double>(null);
+        }
+
+        [TestMethod]
+        public void CannotConvertNullToInt32()
+        {
+            CannotConvert<int>(null);
+        }
+
+        [TestMethod]
+        public void CanConvertNullToNullableInt32()
+        {
+            CanConvert<int?>(null, null);
+        }
+
+        [TestMethod]
+        public void CanConvertNullToNullableDouble()
+        {
+            CanConvert<double?>(null, null);
+        }
+
+        [TestMethod]
+        public void CanConvertValidStringToChar()
+        {
+            CanConvert<char>("q", 'q');
+        }
+
+        [TestMethod]
+        public void CannotConvertInvalidStringToChar()
+        {
+            CannotConvert<char>("qqq");
+        }
+
+        [TestMethod]
+        public void CannotConvertEmptyStringToChar()
+        {
+            CannotConvert<char>(string.Empty);
+        }
+
+        [TestMethod]
+        public void CannotConvertNullToChar()
+        {
+            CannotConvert<char>(null);
+        }
+
+        [TestMethod]
+        public void CanConvertCharToString()
+        {
+            CanConvert<string>('q', "q");
+        }
+
+        [TestMethod]
+        public void CanConvertCharToInt()
+        {
+            CanConvert<int>('q', (int)'q');
+        }
+
+        [TestMethod]
+        public void CannotConvertCharToDouble()
+        {
+            CannotConvert<double>('q');
+        }
+
+        [TestMethod]
+        public void CanConvertIntOneToBool()
+        {
+            CanConvert<bool>(1, true);
+        }
+
+        [TestMethod]
+        public void CanConvertIntZeroToBool()
+        {
+            CanConvert<bool>(0, false);
+        }
+
+        [TestMethod]
+        public void CanConvertNotZeroIntToBool()
+        {
+            CanConvert<bool>(2, true);
+        }
+
+        [TestMethod]
+        public void CanConvertTrueStringToBool()
+        {
+            CanConvert<bool>("true", true);
+        }
+
+        [TestMethod]
+        public void CanConvertFalseStringToBool()
+        {
+            CanConvert<bool>("false", false);
+        }
+
+        [TestMethod]
+        public void CannotConvertInvalidStringToBool()
+        {
+            CannotConvert<bool>("qqq");
+        }
+
+        [TestMethod]
+        public void CannotConvertNullToBool()
+        {
+            CannotConvert<bool>(null);
+        }
+
+        private void CanConvert<T>(object value, T expectedValue)
+        {
+            var convertedValue = Convert<T>(value, true);
+            Assert.AreEqual(expectedValue, convertedValue);
+        }
+
+        private void CannotConvert<T>(object value)
+        {
+            Convert<T>(value, false);
+        }
+
+        private T Convert<T>(object value, bool expectedResult)
+        {
+            T convertedValue;
+            var converted = ConvertEx.TryConvert<T>(value, out convertedValue);
+            Assert.AreEqual(expectedResult, converted);
+            return convertedValue;
+        }
+    }
+}
---
+++ TrumpSoftware.Common/ConvertEx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace TrumpSoftware.Common
+{
+    public static class ConvertEx
+    {
+        public static bool TryConvert<T>(object value, IFormatProvider formatProvider, out T result)
+        {
+            Type targetType = typeof(T);
+
+            if (value is T)
+            {
+                result = (T)value;
+                return true;
+            }
+
+            var typeInfo = targetType.GetTypeInfo();
+            if (value == null && !typeInfo.IsValueType)
+            {
+                result = (T)value;
+                return true;
+            }
+
+            var nullableUnderlyingType = Nullable.GetUnderlyingType(targetType);
+
+            if (value == null && nullableUnderlyingType != null)
+            {
+                result = (T)value;
+                return true;
+            }
+
+            if (nullableUnderlyingType != null)
+            {
+                targetType = nullableUnderlyingType;
+                typeInfo = nullableUnderlyingType.GetTypeInfo();
+            }
+
+            if (value == null && typeInfo.IsValueType)
+            {
+                result = default(T);
+                return false;
+            }
+
+            if (!typeInfo.IsPrimitive && targetType != typeof(string))
+            {
+                result = default(T);
+                return false;
+            }
+
+            try
+            {
+                result = (T)System.Convert.ChangeType(value, targetType, formatProvider);
+            }
+            catch (Exception)
+            {
+                result = default(T);
+                return false;
+            }
+            return true;
+        }
+
+        public static bool TryConvert<T>(object value, out T result)
+        {
+            return TryConvert(value, CultureInfo.CurrentCulture, out result);
+        }
+
+        public static T Convert<T>(object value, IFormatProvider formatProvider)
+        {
+            T result;
+            if (!TryConvert<T>(value, formatProvider, out result))
+                throw new Exception(string.Format("Can not convert {0} to type '{1}'", GetValueTypeString(value), typeof(T)));
+            return result;
+        }
+
+        public static T Convert<T>(object value)
+        {
+            return Convert<T>(value, CultureInfo.CurrentCulture);
+        }
+
+        private static string GetValueTypeString(object value)
+        {
+            if (value == null)
+                return "'null'";
+            return string.Format("value of type '{0}'", value.GetType());
+        }
+    }
+}
