27e77c9055415dcab5e3aebaecb2224a33857fd9
Egor Novikov <novikov.ea@mail.com>
2016-01-20 21:47:24 +0300

    AddOrRemove & AddOrRemoveOrUpdate methods implemented

--- TrumpSoftware.Common/Extensions/EnumerableExtentions.cs
+++ TrumpSoftware.Common/Extensions/EnumerableExtentions.cs
@@ -6,10 +6,10 @@ using TrumpSoftware.Common.Helpers;
 
 namespace TrumpSoftware.Common.Extensions
 {
-	public static class EnumerableExtentions
-	{
-		public static IEnumerable<T> GetRandom<T>(this IEnumerable<T> source, int count)
-		{
+    public static class EnumerableExtentions
+    {
+        public static IEnumerable<T> GetRandom<T>(this IEnumerable<T> source, int count)
+        {
             if (source == null)
                 throw new ArgumentNullException("source");
             if (count < 0)
@@ -17,15 +17,15 @@ namespace TrumpSoftware.Common.Extensions
             if (source.Count() < count)
                 throw new ArgumentException("Requested count is greater than existing", "source");
             var list = source.ToList();
-			var result = new List<T>();
-			for (int i = 0; i < count; i++)
-			{
-				int number = RandomHelper.GetInt(list.Count - 1);
-				result.Add(list[number]);
-				list.RemoveAt(number);
-			}
-			return result;
-		}
+            var result = new List<T>();
+            for (int i = 0; i < count; i++)
+            {
+                int number = RandomHelper.GetInt(list.Count - 1);
+                result.Add(list[number]);
+                list.RemoveAt(number);
+            }
+            return result;
+        }
 
         public static T GetRandom<T>(this IEnumerable<T> source)
         {
@@ -36,50 +36,50 @@ namespace TrumpSoftware.Common.Extensions
             return source.GetRandom(1).Single();
         }
 
-		public static IEnumerable<T> Mix<T>(this IEnumerable<T> source)
-		{
-		    if (source == null)
-		        throw new ArgumentNullException("source");
-		    return source.ToDictionary(item => RandomHelper.GetInt())
+        public static IEnumerable<T> Mix<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            return source.ToDictionary(item => RandomHelper.GetInt())
                 .OrderBy(x => x.Key)
                 .Select(x => x.Value)
                 .ToArray();
-		}
+        }
 
-	    public static IEnumerable<T> GetSelfOrEmpty<T>(this IEnumerable<T> source)
-	    {
-	        return source ?? Enumerable.Empty<T>();
-	    }
+        public static IEnumerable<T> GetSelfOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source ?? Enumerable.Empty<T>();
+        }
 
-	    public static int GetCountOrZero(this IEnumerable source)
-	    {
-	        source = source ?? Enumerable.Empty<object>();
-	        return source.OfType<object>().Count();
-	    }
+        public static int GetCountOrZero(this IEnumerable source)
+        {
+            source = source ?? Enumerable.Empty<object>();
+            return source.OfType<object>().Count();
+        }
 
-	    public static IEnumerable<T> Union<T>(this IEnumerable<T> source, params T[] items)
-	    {
+        public static IEnumerable<T> Union<T>(this IEnumerable<T> source, params T[] items)
+        {
             return Enumerable.Union(source, items);
-	    }
+        }
 
         public static Stack<T> ToStack<T>(this IEnumerable<T> source)
-	    {
-	        return new Stack<T>(source);
-	    }
+        {
+            return new Stack<T>(source);
+        }
 
         public static Queue<T> ToQueue<T>(this IEnumerable<T> source)
         {
             return new Queue<T>(source);
         }
 
-	    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
-	    {
-	        if (action == null)
-	            throw new ArgumentNullException("action");
-	        foreach (var item in source)
-	        {
-	            action(item);
-	        }
-	    }
-	}
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (action == null)
+                throw new ArgumentNullException("action");
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
 }
--- TrumpSoftware.Common/Extensions/ListExtensions.cs
+++ TrumpSoftware.Common/Extensions/ListExtensions.cs
@@ -1,22 +1,138 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TrumpSoftware.Common.Extensions
 {
     public static class ListExtensions
     {
-        public static void Sort<T>(this IList<T> sourceCollection, IComparer<T> comparer)
+        public static void Sort<T>(this ICollection<T> source, IComparer<T> comparer)
         {
-            var list = new List<T>(sourceCollection);
+            var list = new List<T>(source);
             list.Sort(comparer);
-            sourceCollection.Clear();
+            source.Clear();
             foreach (var item in list)
-                sourceCollection.Add(item);
+                source.Add(item);
         }
 
-        public static void AddRange<T>(this IList<T> sourceCollection, IEnumerable<T> newItems)
+        public static void AddRange<T>(this ICollection<T> source, IEnumerable<T> newItems)
         {
             foreach (var newItem in newItems)
-                sourceCollection.Add(newItem);
+                source.Add(newItem);
+        }
+
+        public static void RemoveRange<T>(this ICollection<T> source, IEnumerable<T> removingItems)
+        {
+            foreach (var newItem in removingItems)
+                source.Remove(newItem);
+        }
+
+        public static void AddOrRemoveOrUpdate<TItem, TNewItem, TKey>(this ICollection<TItem> source,
+                                                                      IEnumerable<TNewItem> newItems,
+                                                                      Func<TItem, TKey> getItemKey,
+                                                                      Func<TNewItem, TKey> getNewItemKey,
+                                                                      Func<TNewItem, TItem> getItemFunc,
+                                                                      Action<TItem, TNewItem> updateItemAction,
+                                                                      IEqualityComparer<TKey> comparer = null)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (getItemKey == null)
+                throw new ArgumentNullException("getItemKey");
+            if (getNewItemKey == null)
+                throw new ArgumentNullException("getNewItemKey");
+            if (getItemFunc == null)
+                throw new ArgumentNullException("getItemFunc");
+
+            comparer = comparer ?? EqualityComparer<TKey>.Default;
+            if ((newItems == null || !newItems.Any()) && source.Any())
+            {
+                source.Clear();
+                return;
+            }
+
+            var oldItemsByKey = source.ToLookup(getItemKey, comparer).ToDictionary(x => x.Key, x => x.FirstOrDefault());
+            var newItemsByKey = newItems.ToLookup(getNewItemKey, comparer).ToDictionary(x => x.Key, x => x.FirstOrDefault());
+
+            var itemsToDelete = oldItemsByKey.Where(x => !newItemsByKey.Keys.Contains(x.Key, comparer)).Select(x => x.Value).ToList();
+            var itemsToAdd = newItemsByKey.Where(x => !oldItemsByKey.Keys.Contains(x.Key)).Select(x => getItemFunc(x.Value)).ToList();
+
+            var oldAndNewItemPairsToUpdate = new List<KeyValuePair<TItem, TNewItem>>();
+            if (updateItemAction != null)
+                oldAndNewItemPairsToUpdate = newItemsByKey.Where(x => oldItemsByKey.Keys.Contains(x.Key)).Select(x => new KeyValuePair<TItem, TNewItem>(oldItemsByKey[x.Key], x.Value)).ToList();
+
+            source.RemoveRange(itemsToDelete);
+            source.AddRange(itemsToAdd);
+
+            if (updateItemAction != null)
+            {
+                foreach (var pair in oldAndNewItemPairsToUpdate)
+                {
+                    var oldItem = pair.Key;
+                    var newItem = pair.Value;
+                    if (!Equals(oldItem, null) && !Equals(newItem, null))
+                    {
+                        updateItemAction(oldItem, newItem);
+                    }
+                }
+            }
+        }
+
+        public static void AddOrRemoveOrUpdate<TItem, TNewItem>(this ICollection<TItem> source,
+                                                                IEnumerable<TNewItem> newItems,
+                                                                Func<TNewItem, TItem> newItemToOldItemFunc,
+                                                                Action<TItem, TNewItem> updateItemAction,
+                                                                IEqualityComparer<TItem> comparer = null)
+        {
+            Func<TNewItem, TItem> createItemAction = x =>
+            {
+                var item = newItemToOldItemFunc(x);
+                updateItemAction(item, x);
+                return item;
+            };
+            AddOrRemoveOrUpdate(source, newItems, x => x, newItemToOldItemFunc, createItemAction, updateItemAction, comparer);
+        }
+
+        public static void AddOrRemoveOrUpdate<T>(this ICollection<T> source,
+                                                  IEnumerable<T> newItems,
+                                                  Action<T, T> updateItemAction,
+                                                  IEqualityComparer<T> comparer = null)
+        {
+            AddOrRemoveOrUpdate(source, newItems, x => x, updateItemAction, comparer);
+        }
+
+        public static void AddOrRemove<TItem, TNewItem>(this ICollection<TItem> source,
+                                                        IEnumerable<TNewItem> newItems,
+                                                        Func<TNewItem, TItem> getItemFunc,
+                                                        IEqualityComparer<TItem> comparer = null)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (getItemFunc == null)
+                throw new ArgumentNullException("getItemFunc");
+
+            var convertedNewItems = newItems.Select(getItemFunc).ToList();
+
+            var itemsToDelete = source.Except(convertedNewItems, comparer).ToList();
+            var itemsToAdd = convertedNewItems.Except(source, comparer).ToList();
+
+            source.RemoveRange(itemsToDelete);
+            source.AddRange(itemsToAdd);
+        }
+
+        public static void AddOrRemove<T>(this ICollection<T> source,
+                                          IEnumerable<T> newItems,
+                                          IEqualityComparer<T> comparer = null)
+        {
+            AddOrRemove(source, newItems, x => x, comparer);
+        }
+
+        public static void RemoveIf<T>(ICollection<T> source, Func<T, bool> condition)
+        {
+            foreach (var item in source.Where(condition))
+            {
+                source.Remove(item);
+            }
         }
     }
 }
