237a6f55a6a5f0ed91ccbf163d77f4161eb339d6
Egor Novikov <novikov.ea@mail.com>
2016-01-24 16:38:59 +0300

    Value markup extension implemented

---
+++ TrumpSoftware.Wpf/MarkupExtensions/Value.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Markup;
+using TrumpSoftware.Common;
+
+namespace TrumpSoftware.Wpf.MarkupExtensions
+{
+    public class Value : MarkupExtension
+    {
+        #region Fields
+
+        private readonly object _value;
+        private readonly Type _type;
+
+        #endregion
+
+        #region Ctor
+
+        public Value(object value, Type type)
+        {
+            _value = value;
+            _type = type;
+        }
+
+        #endregion
+
+        public IFormatProvider FormatProvider { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ConvertEx.Convert(_value, _type, FormatProvider);
+        }
+    }
+}
