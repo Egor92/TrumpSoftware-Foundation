9a3b6dec38e973196b28b2319374cedee0fd3d3a
Egor Novikov <novikov.ea@mail.com>
2016-03-02 00:46:58 +0300

    TypeCase & RangeCase implemented

---
+++ TrumpSoftware.Shared/Converters/Cases/RangeCase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TrumpSoftware.Common;
+
+#if WPF
+namespace TrumpSoftware.Wpf.Converters.Cases
+#elif WINRT
+namespace TrumpSoftware.WinRT.Converters.Cases
+#endif
+{
+    public class RangeCase : ICase
+    {
+        #region Ctor
+
+        public RangeCase()
+        {
+            Min = Double.MinValue;
+            Max = Double.MaxValue;
+        }
+
+        #endregion
+
+        public double Min { get; set; }
+
+        public double Max { get; set; }
+        public bool IsMinStrictly { get; set; }
+        public bool IsMaxStrictly { get; set; }
+
+        public object Value { get; set; }
+
+        public bool IsMatched(object value)
+        {
+            double @double;
+            if (!ConvertEx.TryConvert(value, out @double))
+                return false;
+            return (IsMinStrictly && Min <= @double || !IsMinStrictly && Min < @double)
+                   && (IsMaxStrictly && Max >= @double || !IsMaxStrictly && Max > @double);
+        }
+    }
+}
---
+++ TrumpSoftware.Shared/Converters/Cases/TypeCase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+#if WINRT
+using TrumpSoftware.WinRT.Extensions;
+#endif
+
+#if WPF
+namespace TrumpSoftware.Wpf.Converters.Cases
+#elif WINRT
+
+namespace TrumpSoftware.WinRT.Converters.Cases
+#endif
+{
+    public class TypeCase : ICase
+    {
+        public Type Type { get; set; }
+
+        public bool IsStrictly { get; set; }
+
+        public object Value { get; set; }
+
+        public bool IsMatched(object value)
+        {
+            if (Type == null || value == null)
+                return false;
+            if (IsStrictly)
+            {
+                return Type == value.GetType();
+            }
+            else
+            {
+                return Type.IsInstanceOfType(value);
+            }
+        }
+    }
+}
