00775ac1b10dbd952b57afbbbb456007ca9e6e99
Egor Novikov <novikov.ea@mail.com>
2016-04-14 00:56:32 +0300

    Result class implemented #1

---
+++ TrumpSoftware.Common/Result.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace TrumpSoftware.Common
+{
+    [DebuggerDisplay("Result: IsSuccess={IsSuccess}, Message={Message}")]
+    public class Result
+    {
+        public bool IsSuccess { get; private set; }
+
+        public string Message { get; private set; }
+
+        public Result()
+        {
+            IsSuccess = false;
+        }
+
+        public Result(bool isSuccess, string message = null)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+    }
+
+    [DebuggerDisplay("Result: IsSuccess={IsSuccess}, Message={Message}, Data={Data != null ? Data.GetType() : null}")]
+    public class Result<T> : Result
+    {
+        public T Data { get; private set; }
+    }
+}
